395df900270bea667b212e81cd5330e01e9f9a7f
bismark <bismark.john.anes@gmail.com>
2019-04-24 14:57:35 +0700

    added sequelize

--- config/default.js
+++ config/default.js
@@ -2,5 +2,19 @@ module.exports = {
   app: {
     hostname: process.env.HOSTNAME,
     port: process.env.PORT,
-  }
+  },
+  db: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DRIVER,
+    database: process.env.DB_NAME,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    define: {
+      timestamps: false
+    },
+    dialectOptions: {
+      decimalNumbers: true,
+    }
+  },
 };
